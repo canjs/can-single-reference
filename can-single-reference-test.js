d108a597a9e46d258d83b926da491a46562669f3
Justin Meyer <justinbmeyer@gmail.com>
2018-05-17 20:03:08 -0500

    first commit

---
+++ can-single-reference-test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var QUnit = require('steal-qunit');
+var singleReference = require('./can-single-reference');
+
+QUnit.module("can-single-reference");
+
+QUnit.test("basics", function(assert){
+  var obj = {};
+  singleReference.set(obj, 'pet', 'dog');
+  var retrieved = singleReference.getAndDelete(obj, 'pet');
+  assert.equal(retrieved, 'dog', 'sets and retrieves successfully');
+  assert.equal(Object.keys(obj).length, 0, 'also deletes when retrieved');
+});
